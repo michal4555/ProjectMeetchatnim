af80e8673469a05c5e75c673a01d2d49e1d43361
michal4555 <landman6200@gmail.com>
2024-11-21 01:25:32 +0200

    my commit message

---
+++ meet/Controllers/MatchmakerController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace meet.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MatchmakerController : ControllerBase
+    {
+        // GET: api/<MatchmakerController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<MatchmakerController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<MatchmakerController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<MatchmakerController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<MatchmakerController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
